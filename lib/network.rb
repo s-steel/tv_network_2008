9d4e29acbed8f9a381a1dafbcfc2f5a6ff99f172
Sean Steel <seanrsteel@gmail.com>
2020-09-01 10:49:27 -0600

    Create main_characters and actors_by_show methods and tests; WIP

--- lib/network.rb
+++ lib/network.rb
@@ -9,5 +9,25 @@ class Network
     @shows << show
   end
 
-  
+  def main_characters
+    # char = []
+    # shows.find_all do |show|
+    #   char << show.characters
+    # end
+    # char.first
+    shows.select do |show|
+      show.highest_paid_actor
+    end
+  end
+
+  def actors_by_show
+    actor_show = Hash.new
+    shows.map do |show|
+      char = show.characters
+      actor_show[show] << char.name
+
+    end
+
+
+  end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -34,5 +34,35 @@ class NetworkTest < MiniTest::Test
     assert_equal [knight_rider, parks_and_rec], nbc.shows
   end
 
+  def test_main_characters
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+# require 'pry'; binding.pry
+    assert_equal [michael_knight, leslie_knope], nbc.main_characters
+  end
+
+  def test_actors_by_show
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    test_hash = {knight_rider: ["David Hasselhoff", "William Daniels"], parks_and_rec: ["Amy Poehler", "Nick Offerman"]}
+#    assert_equal test_hash, nbc.actors_by_show
+  end
 
 end
