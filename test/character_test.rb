9b08a03fac1fae154818803fbf5432c49cf7e2f9
Sean Steel <seanrsteel@gmail.com>
2020-09-01 09:17:04 -0600

    Fix attribute test

--- test/character_test.rb
+++ test/character_test.rb
@@ -3,17 +3,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class CharacterTest < MiniTest::Test
-  def setup
-    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
-  end
   def test_it_exists
-
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     assert_instance_of Character, kitt
   end
 
   def test_has_attributes
-
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+require 'pry'; binding.pry
     assert_equal "KITT", kitt.name
     assert_equal "William Daniels", kitt.actor
     assert_equal 1000000, kitt.salary
